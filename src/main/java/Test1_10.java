2b188d8c571e879e2d447115082bc12c6f242dbd
aejeong <aejeong-context@kakao.com>
2021-10-16 21:05:50 +0900

    4일차 : 2문제
    2.가장 짧은 문자거리
    '최소거리' 이기 때문에 for문을 2번 돌려준다. 왼쪽에서 한번 최소거리를 구한 후 해당 result를 갖고 오른쪽에서 한번 더 최소 거리를 구한다.
    1. 첫번째 for문에서 문자 e가 같으면 0 틀리면 tmp++ 를 해주어 result 배열에 대입한다.
    2. 두번째 for문에서 제일 뒤에서 부터 e가 맞은지 다시 확인하며 tmp = 0을 대입하고 +1을 해주고 틀리면 tmp 의 숫자가 result[i] 보다 크면 덮어 쓴다.
    
    * tmp는 자릿수 대로 증가 하고 e를 만나면 0으로 다시 초기화 한다.

--- src/main/java/Test1_10.java
+++ src/main/java/Test1_10.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
+
 public class Test1_10 {
+  public int[] solution(String s, char t) {
+    int[] result = new int[s.length()];
+
+    int tmp = 0;
+
+    // 왼쪽으로 부터 떨어진 거리를 구한다.
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == t) {
+        tmp = 0;
+      } else {
+        tmp++;
+      }
+      result[i] = tmp;
+    }
+
+    // 오른쪽으로 부터 떨어진 거리를 구한다.
+    tmp = 0;
+    for (int i = s.length() - 1; i >= 0; i--) {
+      if (s.charAt(i) == t) {
+        tmp = 0;
+      } else if(result[i]>tmp){
+        // 둘중 작은 숫자 대입
+        result[i] =tmp;
+      }
+        tmp++;
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    Test1_10 main = new Test1_10();
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    char t = sc.next().charAt(0);
+    for (int x : main.solution(s, t)) {
+      System.out.print(x);
+    }
+  }
 }
