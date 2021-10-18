af3db52e9a205711325ab782a31fa87a2f22c43d
aejeong <aejeong-context@kakao.com>
2021-10-18 17:09:23 +0900

    6일차 Array : 2-1 큰 수 출력하기
    ArrayList 를 사용 하여 첫 번째는 무조건 출력 후
    앞 뒤 비교 하였다. 앞보다 크면 추가되므로 항상 뒤에 있는 인자가 추가됨으로 i+1 번째 가 추가된다

---
+++ src/main/java/Test2_01.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Test2_01 {
+
+  public ArrayList<Integer> solution(int[] numberArray) {
+    ArrayList<Integer> result = new ArrayList<>();
+    result.add(numberArray[0]);
+    for (int i = 0; i < numberArray.length-1; i++) {
+      System.out.println(numberArray[i]);
+      System.out.println(numberArray[i+1]);
+      if(numberArray[i]<numberArray[i+1]){
+        result.add(numberArray[i+1]);
+      }
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    Test2_01 main = new Test2_01();
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int[] numberArray = new int[num];
+    for (int i = 0; i < num; i++) {
+      numberArray[i] = sc.nextInt();
+    }
+    for (int n : main.solution(numberArray)) {
+      System.out.print(n + " ");
+    }
+  }
+}
