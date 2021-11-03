6b036499e4367b9633e496cf44dae70975f04e6a
aejeong <aejeong-context@kakao.com>
2021-11-03 23:10:59 +0900

    9일차 Array : 2-10 봉우리
    1. 배열에 방향을 먼저 담아 두고 for문을 돌린다.
    2. 해당 위치에 있는 상하좌우를 보기 위해 i,j 에 + 를 해주었다.
    3. 밖에 있는 숫자들은 상하좌우를 보면 Index가 - 되거나 오버되기 때문에 if문에서 미리 걸러주어 indexOfBind 에러가 나지 않도록 한다.

---
+++ src/main/java/Test02_10.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Test02_10 {
+
+  public int solution(int[][] numberArray, int num) {
+    // 상하좌우
+    int[] x = {-1, 0, 1, 0};
+    int[] y = {0, 1, 0, -1};
+    int result = 0;
+
+    for (int i = 0; i < num; i++) {
+      for (int j = 0; j < num; j++) {
+        boolean check = true;
+        for (int k = 0; k < 4; k++) {
+          int targetX = i + x[k];
+          int targetY = j + y[k];
+          if (targetX >= 0
+              && targetX < num
+              && targetY >= 0
+              && targetY < num
+              && numberArray[i][j] <= numberArray[targetX][targetY]) {
+            check = false;
+            break;
+          }
+        }
+        if (check) result++;
+      }
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    Test02_10 main = new Test02_10();
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int[][] numberArray = new int[num][num];
+    for (int i = 0; i < num; i++) {
+      for (int j = 0; j < num; j++) {
+        numberArray[i][j] = sc.nextInt();
+      }
+    }
+    System.out.println(main.solution(numberArray, num));
+  }
+}
